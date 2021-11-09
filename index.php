7cb0704f6ad19d0faeac98946acb80544638becc
drajin <drajin@gmail.com>
2021-11-09 03:21:23 +0100

    header, footer

--- index.php
+++ index.php
@@ -29,6 +29,48 @@
     </div>
 </nav>
 
+<!--footer-->
+<footer>
+<div class="container">
+    <div class="col">
+        <h3>Top Products</h3>
+        <ul>
+            <li><a href="">Phones</a></li>
+            <li><a href="">Clothes</a></li>
+            <li><a href="">Accessories</a></li>
+            <li><a href="">Digital Art</a></li>
+        </ul>
+    </div>
+    <div class="col">
+        <h3>Quick Links</h3>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Shop</a></li>
+            <li><a href="">Products</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>
+    <div class="col">
+        <h3>Newsletter</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet cupiditate delectus in modi. Aperiam cum dolorum eaque error quas quia quisquam rem sapiente voluptas.</p>
+        <input type="email" placeholder="Enter email">
+        <input type="submit" value="Subscribe">
+    </div>
+</div>
+</footer>
+<!--    bottom bar-->
+    <section class="bottomBar">
+        <p>&copy; 2020. Webshop</p>
+        <div class="socials">
+            <a href=""><i class="fab fa-facebook-square"></i></a>
+            <a href=""><i class="fab fa-instagram"></i></a>
+            <a href=""><i class="fab fa-pinterest"></i></a>
+        </div>
+    </section>
+
+
+
+
 <script src="main.js"></script>
 </body>
 </html>
