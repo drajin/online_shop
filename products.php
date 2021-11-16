2f1b22e050c25f9f3267b8f23d6263d10ac042c3
drajin <drajin@gmail.com>
2021-11-16 02:36:56 +0100

    products, responsive

--- products.php
+++ products.php
@@ -5,4 +5,30 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 </section>
 
+<section class="products container">
+    <article>
+        <img src="img/products1.jpg" alt="">
+    </article>
+    <aside>
+        <h3>Nike Shoes</h3>
+        <h4>$139.00</h4>
+        <p>Category: <span>Shoes</span></p>
+        <p>Availability: <span>In stock</span></p>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cupiditate delectus deserunt doloremque ea esse laudantium, minus molestias nobis quisquam tenetur totam unde! Architecto deleniti dignissimos eius est eveniet, explicabo illum officiis? Dolorem eius eveniet molestiae perferendis quas sapiente tenetur.</p>
+        <form action="">
+            <label for="">Quantity</label>
+            <select name="" id="">
+                <option value="quantity">1</option>
+                <option value="quantity">2</option>
+                <option value="quantity">3</option>
+            </select>
+            <br>
+            <button>Add to cart</button>
+            <span><i class="far fa-gem"></i></span>
+            <span><i class="far fa-heart"></i></span>
+        </form>
+    </aside>
+</section>
+
 <?php include 'partials/footer.php' ?>
