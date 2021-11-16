e191469f6bc923cbc2a8ef131e8ba165f070edc0
drajin <drajin@gmail.com>
2021-11-16 01:42:29 +0100

    shop

--- products.php
+++ products.php
@@ -1,5 +1,8 @@
 <?php include 'partials/header.php' ?>
 
-
+<section class="headers">
+    <h2>Products</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</section>
 
 <?php include 'partials/footer.php' ?>
--- shop.php
+++ shop.php
@@ -1,5 +1,43 @@
 <?php include 'partials/header.php' ?>
 
-<h1 style="font-size: 7rem;">Shop</h1>
+<section class="headers">
+    <h2>Shop</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</section>
+
+<!--security-->
+<section class="security container">
+    <div class="col">
+        <i class="fas fa-hand-holding-usd"></i>
+        <h3>Money back guarantee</h3>
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+    <div class="col">
+        <i class="fas fa-truck"></i>
+        <h3>Free delivery</h3>
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+    <div class="col">
+        <i class="fas fa-headset"></i>
+        <h3>Always support</h3>
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+    <div class="col">
+        <i class="fas fa-money-check-alt"></i>
+        <h3>Secure payments</h3>
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+</section>
+
+<!--offer-->
+<section class="offer">
+    <div class="container">
+        <h4>Women's Collection</h4>
+        <h2>50% OFF</h2>
+        <button>Discover Now</button>
+        <p>Limited Time Offer</p>
+
+    </div>
+</section>
 
 <?php include 'partials/footer.php' ?>
